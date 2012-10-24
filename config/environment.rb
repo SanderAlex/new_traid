7e21d3494258a252745d3a428cad60242131deb6
SanderAlex <spiritd@yandex.ru>
2012-10-25 00:46:06 +0400

    lol

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TitanTraid::Application.initialize!
-
-require 'hassle'
-
-Rails::Initializer.run do |config|
-
-     config.middleware.use Hassle
-end
