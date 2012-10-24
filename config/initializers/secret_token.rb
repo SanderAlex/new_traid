492ca4a76febeb04880b1125b0fc03949ab21814
SanderAlex <spiritd@yandex.ru>
2012-10-24 21:09:35 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TitanTraid::Application.config.secret_token = '2f8368e31dd9b985523c0a1f379b947199c3317abc858e11b832a829e20f4b1bf117134d9e9041910433a44acb8d3ac3a6c8d7379015489e6ced53c80c564060'
