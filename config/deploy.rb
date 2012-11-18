d46f5b65c9b967b64b8c8683f0b12e912e93965c
SanderAlex <spiritd@yandex.ru>
2012-11-18 20:35:40 +0400

    hah

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,6 +32,19 @@ require 'bundler/capistrano'
 load 'deploy/assets'
 
 
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ lib/assets | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+end
+
 # Для удобства работы мы рекомендуем вам настроить авторизацию
 # SSH по ключу. При работе capistrano будет использоваться
 # ssh-agent, который предоставляет возможность пробрасывать
