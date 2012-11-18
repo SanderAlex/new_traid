1e8eb82e5bfb9ac70d79569df2b9526601182eb2
SanderAlex <spiritd@yandex.ru>
2012-11-18 20:52:20 +0400

    koko

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,7 +56,7 @@ end
 
 # Имя вашего проекта в панели управления.
 # Не меняйте это значение без необходимости, оно используется дальше.
-set :application,     "titan"
+set :application,     "titantraid"
 
 # Сервер размещения проекта.
 set :deploy_server,   "neon.locum.ru"
